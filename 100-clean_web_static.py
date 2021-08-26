9e4fef04e12450066c7e0ad6ab57b8520944e049
yoyogold-a11 <tmrfack@gmail.com>
2021-08-26 05:07:06 -0500

    cleaner

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -10,29 +10,37 @@ from os.path import exists, isdir
 env.hosts = ['34.73.133.125', '52.201.252.232']
 
 
-def local_clean(numer=0):
-    """Locas Clean"""
-    fd_list = local('ls -1t verions', capture=True)
-    fd_list = fd_list.split('\n')
-    num = int(number)
+def local_clean(number=0):
+    """Fabric script (based on the file 3-deploy_web_static.py)
+    that deletes out-of-date archives, using the function do_clean"""
+    l = local('ls -1t versions', capture=True)
+    l = l.split('\n')
+    n = int(number)
     if n in (0, 1):
         n = 1
-    for i in fd_list[n:]:
-        local('sudo rm versions/' + i)
+    print(len(l[n:]))
+    for i in l[n:]:
+        local('rm versions/' + i)
 
 
-def remote_clean(numer=0):
-    """Remote Clean"""
-    fd_list = run('ls -1t /data/web_static/releases')
-    fd_list = fd_list.split('\r\n')
-    numb = int(number)
+def remote_clean(number=0):
+    """Fabric script (based on the file 3-deploy_web_static.py)
+    that deletes out-of-date archives, using the function do_clean"""
+    l = run('ls -1t /data/web_static/releases')
+    l = l.split('\r\n')
+    print(l)
+    n = int(number)
     if n in (0, 1):
         n = 1
-    for i in fd_list[n:]:
-        run('sudo rm -rf /data/web_static/releases/' + i)
+    print(len(l[n:]))
+    for i in l[n:]:
+        if i is 'test':
+            continue
+        run('rm -rf /data/web_static/releases/' + i)
 
 
 def do_clean(number=0):
-    """Fabric script that deletes aout of dates archives"""
+    """Fabric script (based on the file 3-deploy_web_static.py)
+    that deletes out-of-date archives, using the function do_clean"""
     local_clean(number)
     remote_clean(number)
